3bfcc699dcc633ba68f816597a8340e35fa3d7c8
fremag <fremag.gh@gmail.com>
2017-11-17 14:29:42 +0100

    MemoryKeyValueStoreService: do not create a new list every  time GetAllValues is called (perf/memory)

--- Nats.Services.Core/KeyValueStoreService/MemoryKeyValueStoreService.cs
+++ Nats.Services.Core/KeyValueStoreService/MemoryKeyValueStoreService.cs
@@ -6,23 +6,36 @@ namespace Nats.Services.Core.KeyValueStoreService
     public class MemoryKeyValueStoreService<T_Key, T_Value> : AbstractKeyValueStoreService<T_Key, T_Value> where T_Value : IKeyIdentifiable<T_Key>
     {
         Dictionary<T_Key, T_Value> dicoKeyValues = new Dictionary<T_Key, T_Value>();
+        List<T_Value> allValues = null;
         public override List<T_Value> GetAllValues()
         {
-            return dicoKeyValues.Values.ToList();
+            if( allValues == null)
+            {
+                allValues = dicoKeyValues.Values.ToList();
+            }
+            return allValues;
         }
 
         public override void DoInsert(T_Value value)
         {
+            allValues = null;
             dicoKeyValues[value.Key] = value;
         }
 
         public override void DoUpdate(T_Value value)
         {
-            dicoKeyValues[value.Key] = value;
+            if (dicoKeyValues.ContainsKey(value.Key))
+            {
+                dicoKeyValues[value.Key] = value;
+            }
         }
         public override void DoDelete(T_Value value)
         {
-            dicoKeyValues.Remove(value.Key);
+            if (dicoKeyValues.ContainsKey(value.Key))
+            {
+                allValues = null;
+                dicoKeyValues.Remove(value.Key);
+            }
         }
 
         public override T_Value GetByKey(T_Key key)
